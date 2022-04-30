821464db591fcb4460623dd3b377039ee5f93f3d
Chris <chris@verida.io>
2022-04-30 23:41:26 +0930

    More refresh token unit tests. Add access token support. Db refactor.

--- src/components/authManager.js
+++ src/components/authManager.js
@@ -1,15 +1,27 @@
 require('dotenv').config();
 const randtoken = require('rand-token');
 const jwt = require('jsonwebtoken');
-import { DIDClient } from '@verida/did-client'
 const mcache = require("memory-cache")
+
+import { DIDClient } from '@verida/did-client'
+import EncryptionUtils from '@verida/encryption-utils';
+
+import Db from "./db"
 
 let didClient
 
-//import Utils from "./utils";
-
 class AuthManager {
 
+    /**
+     * Generates a pre authorization JWT that must be signed by the 
+     * DID to authenticate.
+     * 
+     * This avoid replay attacks.
+     * 
+     * @param {*} did 
+     * @param {*} contextName 
+     * @returns 
+     */
     generateAuthJwt(did, contextName) {
         const authRequestId = randtoken.generate(256);
         const authJwt = jwt.sign({
@@ -17,7 +29,7 @@ class AuthManager {
             contextName,
             authRequestId
             
-        }, process.env.TOKEN_JWT_SIGN_PK, {
+        }, process.env.REFRESH_JWT_SIGN_PK, {
             // expies in 1 minute
             expiresIn: 60
         })
@@ -25,11 +37,22 @@ class AuthManager {
         return authJwt;
     }
 
+    /**
+     * Verify an authorization request has supplied a valid authorization JWT
+     * and correctly signed the expected string using a valid signature that
+     * matches the DID's public key.
+     * 
+     * @param {*} authJwt 
+     * @param {*} did 
+     * @param {*} contextName 
+     * @param {*} signature 
+     * @returns 
+     */
     async verifyAuthRequest(authJwt, did, contextName, signature) {
         // verify authJwt is valid
         let decodedJwt
         try {
-            decodedJwt = jwt.verify(authJwt, process.env.TOKEN_JWT_SIGN_PK, {
+            decodedJwt = jwt.verify(authJwt, process.env.REFRESH_JWT_SIGN_PK, {
                 subject: did
             })
         } catch (err) {
@@ -80,16 +103,238 @@ class AuthManager {
         }
     }
 
-    async generateRefreshToken(did, contextName) {
-        // generate request token
+    /**
+     * Generate a refresh token.
+     * 
+     * Each token is linked to a specific device which allows the owner to easily
+     * invalidate any refresh tokens gratned to a specific device.
+     * 
+     * @param {*} did DID that is authorized
+     * @param {*} contextName Context name that is authorized
+     * @param {*} requestTokenId Optional token ID (used when regenerating a refresh token)
+     * @param {*} expiry Optional unix epoch for when the token should expire
+     * @returns 
+     */
+    async generateRefreshToken(did, contextName, deviceId, expiry) {
         const requestTokenId = randtoken.generate(256);
-        const token = jwt.sign({
+
+        // Set the token to expire
+        if (!expiry) {
+            expiry = 60 * process.env.REFRESH_TOKEN_EXPIRY
+        }
+
+        const deviceHash = EncryptionUtils.hash(`${did}/${contextName}/${deviceId}`)
+
+        const tokenContent = {
+            id: requestTokenId,
+            deviceId,
             sub: did,
             contextName,
-        }, env.COUCHDB_JWT_SIGN_PK, {
+            type: 'refresh'
+        }
+
+        const token = jwt.sign(tokenContent, process.env.REFRESH_JWT_SIGN_PK, {
             // expiry in minutes for this token
-            expiresIn: 60 * env.JWT_SIGN_EXPIRY
+            expiresIn: expiry
         })
+
+        // Save refresh token in the database
+        const couch = Db.getCouch();
+        const tokenDb = couch.db.use(process.env.DB_REFRESH_TOKENS);
+
+        const tokenRow = {
+            _id: requestTokenId,
+            deviceHash,
+            expiry
+        }
+
+        await tokenDb.insert(tokenRow);
+
+        return token
+    }
+
+    /**
+     * Verify a refresht token is valid:
+     * 
+     * - JWT is valid (private key matches, hasn't expired etc.)
+     * - Is of type `refresh` token
+     * - Has a valid entry in the refresh token database
+     * 
+     * @param {*} refreshToken 
+     * @returns The decoded JWT
+     */
+    async verifyRefreshToken(refreshToken) {
+        // verify refreshToken is valid
+        let decodedJwt
+        try {
+            decodedJwt = jwt.verify(refreshToken, process.env.REFRESH_JWT_SIGN_PK, {
+                type: 'refresh'
+            })
+        } catch (err) {
+            // Handle invalid JWT by rejecting verification
+            if (err.name == "JsonWebTokenError") {
+                return false
+            }
+            
+            // Throw unknown error
+            throw err
+        }
+
+        // check this refresh token is in the database (hasn't been invalidated)
+        const couch = Db.getCouch();
+        const tokenDb = couch.db.use(process.env.DB_REFRESH_TOKENS);
+
+        try {
+            const tokenRow = await tokenDb.get(decodedJwt.id);
+        } catch (err) {
+            if (err.message == "deleted") {
+                // Token deleted, so no longer valid
+                return false
+            }
+
+            throw err
+        }
+        return decodedJwt
+    }
+
+    async regenerateRefreshToken(refreshToken) {
+        const decodedJwt = await this.verifyRefreshToken(refreshToken)
+        if (!decodedJwt) {
+            return false;
+        }
+
+        await this.invalidateRefreshToken(refreshToken)
+        const newRefreshToken = await this.generateRefreshToken(decodedJwt.sub, decodedJwt.contextName, decodedJwt.deviceId)
+
+        return newRefreshToken
+    }
+
+    async invalidateRefreshToken(refreshToken) {
+        const decodedJwt = await this.verifyRefreshToken(refreshToken)
+
+        if (!decodedJwt) {
+            return false;
+        }
+
+        const couch = Db.getCouch();
+        const tokenDb = couch.db.use(process.env.DB_REFRESH_TOKENS);
+
+        try {
+            const tokenRow = await tokenDb.get(decodedJwt.id);
+            await tokenDb.destroy(tokenRow._id, tokenRow._rev);
+
+            return true;
+        } catch (err) {
+            if (err.message == "deleted") {
+                // Token deleted, so no longer valid
+                return false
+            }
+
+            throw err
+        }
+    }
+
+    /**
+     * Invalidate a token for a combination of DID, contextName and deviceId
+     * 
+     * @param {*} did 
+     * @param {*} contextName 
+     * @param {*} deviceId 
+     * @returns 
+     */
+    async invalidateDeviceId(did, contextName, deviceId) {
+        const deviceHash = EncryptionUtils.hash(`${did}/${contextName}/${deviceId}`)
+        const query = {
+            selector: {
+                deviceHash
+            },
+            fields: [ "deviceHash" ]
+        };
+
+        const couch = Db.getCouch();
+        const tokenDb = couch.db.use(process.env.DB_REFRESH_TOKENS);
+        const tokenRow = await alice.find(query)
+
+        if (!tokenRow) {
+            return false
+        }
+
+        await tokenDb.destroy(tokenRow._id, tokenRow._rev);
+
+        return true;
+    }
+
+    /**
+     * Genereate an access token for a refresh token
+     * 
+     * @param {*} did 
+     * @param {*} refreshToken 
+     * @returns 
+     */
+    async generateAccessToken(did, refreshToken) {
+        const decodedJwt = await this.verifyRefreshToken(did, refreshToken)
+
+        // generate new request token
+        const requestTokenId = randtoken.generate(256);
+        const token = jwt.sign({
+            id: requestTokenId,
+            sub: decodedJwt.did,
+            contextName: decodedJwt.contextName,
+            type: 'access'
+        }, process.env.ACCESS_JWT_SIGN_PK, {
+            // expiry in minutes for this token
+            expiresIn: 60 * process.env.ACCESS_TOKEN_EXPIRY
+        })
+
+        return token
+    }
+
+    /**
+     * Verify an access token is valid
+     * 
+     * @param {*} accessToken 
+     * @returns 
+     */
+    verifyAccessToken(accessToken) {
+        let decodedJwt
+        try {
+            decodedJwt = jwt.verify(accessToken, process.env.ACCESS_JWT_SIGN_PK, {
+                type: 'access'
+            })
+        } catch (err) {
+            // Handle invalid JWT by rejecting verification
+            if (err.name == "JsonWebTokenError") {
+                return false
+            }
+            
+            // Throw unknown error
+            throw err
+        }
+
+        return decodedJwt
+    }
+
+    async initDb() {
+        const couch = Db.getCouch();
+
+        try {
+            await couch.db.create(process.env.DB_REFRESH_TOKENS)
+        } catch (err) {
+            if (err.message.match(/already exists/)) {
+                // Database already exists
+            } else {
+                throw err
+            }
+        }
+
+        const tokenDb = couch.db.use(process.env.DB_REFRESH_TOKENS);
+
+        const indexDef = {
+            index: { fields: ['deviceHash'] },
+            name: 'deviceHash'
+        };
+
+        const response = await tokenDb.createIndex(indexDef);
     }
 
 }
--- test/auth-manager.js
+++ test/auth-manager.js
@@ -4,7 +4,6 @@ const jwt = require('jsonwebtoken');
 
 import AuthManager from "../src/components/authManager";
 import { AutoAccount } from "@verida/account-node"
-//import EncryptionUtils from "@verida/encryption-utils";
 
 import CONFIG from './config'
 
@@ -20,12 +19,15 @@ describe("AuthManager tests", function() {
 
     describe("Authenticate", async function() {
         let authJwt, decodedJwt, DID
+        let refreshToken, accessToken
 
         it("Generates AuthJWT", async () => {
+            await AuthManager.initDb()
+
             DID = await account.did()
             authJwt = AuthManager.generateAuthJwt(DID, CONTEXT_NAME)
             assert.ok(authJwt, 'Have a token response')
-            decodedJwt = jwt.verify(authJwt, process.env.TOKEN_JWT_SIGN_PK)
+            decodedJwt = jwt.verify(authJwt, process.env.REFRESH_JWT_SIGN_PK)
 
             assert.equal(DID, decodedJwt.sub, "Subject matches the DID")
             assert.equal(CONTEXT_NAME, decodedJwt.contextName, "Correct context name")
@@ -58,5 +60,68 @@ describe("AuthManager tests", function() {
             const invalidSignature = await AuthManager.verifyAuthRequest(authJwt, DID, CONTEXT_NAME, "")
             assert.equal(invalidSignature, false, "Invalid signature")
         })
+
+        it("Generates refresh token", async () => {
+            refreshToken = await AuthManager.generateRefreshToken(DID, CONTEXT_NAME)
+            assert.ok(refreshToken && refreshToken.length, "Refresh token returned")
+
+            const isValid = await AuthManager.verifyRefreshToken(refreshToken)
+            assert.equal(isValid !== false, true, "Refresh token is valid")
+
+            decodedJwt = jwt.verify(refreshToken, process.env.REFRESH_JWT_SIGN_PK)
+            assert.ok(decodedJwt, "Decoded refresh token")
+
+            assert.equal(decodedJwt.contextName, CONTEXT_NAME, "Context name is correct")
+            assert.equal(decodedJwt.type, 'refresh', "Correct token type")
+        })
+
+        it("Generates access token", async () => {
+            accessToken = await AuthManager.generateAccessToken(refreshToken)
+            assert.ok(accessToken && accessToken.length, "Access token returned")
+
+            const isValid = await AuthManager.verifyAccessToken(accessToken)
+            assert.equal(isValid !== false, true, "Access token is valid")
+
+            decodedJwt = jwt.verify(accessToken, process.env.ACCESS_JWT_SIGN_PK)
+            assert.ok(decodedJwt, "Decoded access token")
+
+            assert.equal(decodedJwt.contextName, CONTEXT_NAME, "Context name is correct")
+            assert.equal(decodedJwt.type, 'access', "Correct token type")
+        })
+
+        it("Regenerates refresh token", async () => {
+            const newToken = await AuthManager.regenerateRefreshToken(refreshToken)
+            assert.ok(newToken && newToken.length, "Refresh token returned")
+
+            const isValid = await AuthManager.verifyRefreshToken(newToken)
+            assert.equal(isValid !== false, true, "Refresh token is valid")
+
+            const newDecodedJwt = jwt.verify(refreshToken, process.env.REFRESH_JWT_SIGN_PK)
+            assert.ok(newDecodedJwt, "Decoded access token")
+
+            assert.equal(newDecodedJwt.contextName, CONTEXT_NAME, "Context name is correct")
+            assert.equal(newDecodedJwt.type, 'refresh', "Correct token type")
+
+            const oldIsValid = await AuthManager.verifyRefreshToken(refreshToken)
+            assert.equal(oldIsValid === false, true, "Previous refresth token has been revoked")
+        })
+
+        it("Invalidate refresh token", async () => {
+            const token = await AuthManager.generateRefreshToken(DID, CONTEXT_NAME)
+            const isValid = await AuthManager.verifyRefreshToken(token)
+            assert.ok(isValid, "Generated a valid refresh token")
+
+            const invalidated = await AuthManager.invalidateRefreshToken(token)
+            assert.equal(invalidated, true, "Token successfully invalidated")
+
+            const isValid2 = await AuthManager.verifyRefreshToken(token)
+            assert.equal(isValid2 === false, true, "Token is no longer valid")
+        })
+
+        // custom refresh token expiry works
+        // refresh token times out
+        // access token times out
+        // invalidate refresh tokens for a given device Id (did + context + deviceId: 
+        // garbage collection
     })
 })
