9e1d05bdb45f776b93e0c18142141aa15497c089
tahpot <chris.were@gmail.com>
2022-10-24 18:09:24 +1030

    Fix server tests. Add support for deleting all databases for a user. (#35)

--- test/config.js
+++ test/config.js
@@ -2,10 +2,15 @@ import { EnvironmentType } from "@verida/account"
 require('dotenv').config();
 
 export default {
-    DID_SERVER_URL: undefined,// 'http://localhost:5001',
+    DID_CLIENT_CONFIG: {
+        networkPrivateKey: '',
+        callType: 'web3',
+        web3Config: {},
+        rpcUrl: '
+    },
     ENVIRONMENT: EnvironmentType.TESTNET,
     SERVER_URL: `http://localhost:${process.env.PORT}`,
-    VDA_PRIVATE_KEY: '0x09d3b996ec98a9a536efdffbae40e5eaaf117765a587483c69195c9460165c31',
+    VDA_PRIVATE_KEY: '0x19d3b996ec98a9a536efdffbae41e5eaaf117765a587483c69195c9460165c33',
     CONTEXT_NAME: 'Verida Storage Node Test: Test Application 1',
     DATABASE_SERVER: 'https://db.testnet.verida.io:5002/',  // http://localhost:5000/ for local testing when running local @verida/storage-node
     MESSAGE_SERVER: 'https://db.testnet.verida.io:5002/',  // http://localhost:5000/ for local testing when running local @verida/storage-node
