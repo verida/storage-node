bf53c573e8230b74b8e5290fa6d4acb4c7b8cfc4
Chris <chris@verida.io>
2022-11-04 09:32:24 +1030

    Update config to use testnet storage node

--- test/config.js
+++ test/config.js
@@ -3,25 +3,25 @@ require('dotenv').config();
 
 export default {
     DID_CLIENT_CONFIG: {
-        networkPrivateKey: '',
         callType: 'web3',
-        web3Config: {},
-        rpcUrl: '
+        web3Config: {
+            privateKey: ''
+        },
     },
     ENVIRONMENT: EnvironmentType.TESTNET,
-    SERVER_URL: `http://localhost:${process.env.PORT}`,
-    VDA_PRIVATE_KEY: '0x19d3b996ec98a9a536efdffbae41e5eaaf117765a587483c69195c9460165c33',
+    SERVER_URL: `https://sn-acacia1.tn.verida.tech`,
+    VDA_PRIVATE_KEY: '0x19d3b996ec98a9a536efdffbae41e5eaaf117765a587483c69195c9460165c34',
     CONTEXT_NAME: 'Verida Storage Node Test: Test Application 1',
-    DATABASE_SERVER: 'https://db.testnet.verida.io:5002/',  // http://localhost:5000/ for local testing when running local @verida/storage-node
-    MESSAGE_SERVER: 'https://db.testnet.verida.io:5002/',  // http://localhost:5000/ for local testing when running local @verida/storage-node
+    DATABASE_SERVER: 'https://sn-acacia1.tn.verida.tech/',  // http://localhost:5000/ for local testing when running local @verida/storage-node
+    MESSAGE_SERVER: 'https://sn-acacia1.tn.verida.tech/',  // http://localhost:5000/ for local testing when running local @verida/storage-node
     DEFAULT_ENDPOINTS: {
         defaultDatabaseServer: {
             type: 'VeridaDatabase',
-            endpointUri: 'https://db.testnet.verida.io:5002/'
+            endpointUri: 'https://sn-acacia1.tn.verida.tech/'
         },
         defaultMessageServer: {
             type: 'VeridaMessage',
-            endpointUri: 'https://db.testnet.verida.io:5002/'
+            endpointUri: 'https://sn-acacia1.tn.verida.tech/'
         },
     },
     TEST_DEVICE_ID: 'Unit test device'
