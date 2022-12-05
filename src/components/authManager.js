9e29a1af15ef8e0c697ef05cef1d8f3480667551
Chris <chris@verida.io>
2022-12-06 09:53:13 +1030

    Add console errors and warnings to help with debugging

--- src/components/authManager.js
+++ src/components/authManager.js
@@ -71,6 +71,7 @@ class AuthManager {
             }
             
             // Throw unknown error
+            console.error(`Unable to verify JWT: ${err.message}`)
             throw err
         }
 
@@ -108,6 +109,7 @@ class AuthManager {
             const result = didDocument.verifySig(consentMessage, signature)
 
             if (!result) {
+                console.warning('Invalid signature when verifying signed consent message')
                 // Invalid signature
                 return false
             }
@@ -116,6 +118,7 @@ class AuthManager {
         } catch (err) {
             // @todo: Log error
             // Likely unable to resolve DID or invalid signature
+            console.warning(`Unable to resolve DID or invalid signature: ${err.message}`)
             return false
         }
     }
